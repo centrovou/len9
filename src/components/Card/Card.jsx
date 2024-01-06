f0c7973ba4caf55ae22fcff1c58e2a5bc7b95fa7
centrovou <dima.temnov.00@bk.ru>
2024-01-06 05:29:48 +0300

    7
    7

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -23,7 +23,11 @@ const Card = ({ id, title, price, imageUrl, onPlus }) => {
             className={styles.plus}
             width={30}
             height={30}
-            src={offClick ? 'images/sneakers/btn-cheked.svg' : 'images/sneakers/btn-plus.svg'}
+            src={
+              offClick
+                ? './len9/images/sneakers/btn-cheked.svg'
+                : './len9/images/sneakers/btn-plus.svg'
+            }
             alt="cheked"
           />
         </button>
